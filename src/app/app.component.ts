36bacb46ab78b91c2fb9918c47378e2ca934a379
Letto228 <learnjs@MacBook-Pro-Egor.local>
2022-04-25 19:49:23 +0300

    feat: add header and sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
 	title = 'Angular-learn-js-180422';
 	property = 'property';
+
+	shopIcon =
+		'https://w7.pngwing.com/pngs/479/409/png-transparent-red-and-white-storage-illustration-computer-icons-online-shopping-e-commerce-retail-store-icon.png';
+
+	log(event: Event) {
+		event.stopPropagation();
+		console.log(event);
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { SidenavModule } from './components/sidenav/sidenav.module';
 
+// Module - модуль
+// Directive (Component)
+// Pipe
+
+// Service
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+	declarations: [AppComponent, HeaderComponent], // let/const: Component, Directive, Pipe
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatToolbarModule,
+		MatIconModule,
+		MatButtonModule,
+		SidenavModule,
+	], // import ...
 	providers: [],
 	bootstrap: [AppComponent],
 })
